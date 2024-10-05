f49692ad03db6f3756b627b41aa86d923b5be270
Vishnu Vardhan <vardhan.vishnu557@gmail.com>
2024-10-05 14:23:33 +0530

    Java Programs 1st Part

---
+++ src/day1/FirstProgram.java
@@ -0,0 +1,34 @@
+package day1;
+
+import java.util.*;
+
+public class FirstProgram {
+
+	public static void main(String[] args) {
+//		int a=10, b=20;
+//		
+//		System.out.println("before swapping a="+a+" and b="+b);
+//		
+//		a=(a+b)-(b=a);
+//		
+//		System.out.println("after swapping : a="+a+" and b="+b);
+		
+		
+		List<Integer> list = new ArrayList<Integer>();
+		list.add(1);
+		list.add(1);
+		list.add(2);
+		list.add(2);
+		System.out.println(list);
+		
+		Set set=new HashSet<Integer>(list);
+		System.out.println(set);
+		
+		
+		
+	}
+}
+
+
+
+// https://demo.opencart.com/en-gb?route=account/login ===>> OpenCart
